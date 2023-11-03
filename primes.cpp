7217df6262c6bb5047180076dcf96b5baca8365e
encryptedtheatrics <53976867+encryptedtheatrics@users.noreply.github.com>
2023-11-03 11:51:51 +0000

    feat: added c implementation

---
+++ primes.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+#include <chrono>
+
+bool is_prime(int n) {
+    if (n <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int prime = 0;
+    int notPrime = 0;
+    auto start_time = std::chrono::high_resolution_clock::now();
+    for (int i = 1; i <= 1000000; i++) {
+        if (is_prime(i)) {
+            prime++;
+        } else {
+            notPrime++;
+        }
+    }
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+    std::cout << "Found " << prime << " prime numbers" << std::endl;
+    std::cout << "Found " << notPrime << " non prime numbers" << std::endl;
+    std::cout << "Took " << elapsed_time.count() << " milliseconds." << std::endl;
+    return 0;
+}
